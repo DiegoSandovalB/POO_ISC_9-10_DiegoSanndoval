f39e16d21f497b02b4b3bc26fa21e518f9814e67
DiegoSandovalB <99678295+DiegoSandovalB@users.noreply.github.com>
2022-02-15 15:17:02 -0700

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_holamundo;
+
+/**
+ *
+ * @author DIEGO SANDOVAL BORUNDA
+ * FECHA: 11 FEBRERO 2022   
+ * 
+ */
+public class EVA1_1_HOLAMUNDO { // CUERPO FR UNA CLASE
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // FUNCIÓN
+        // TODO code application logic here
+        
+        System.out.println("Hola mundo");
+    }
+    
+}
